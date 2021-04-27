037ff94c9f36d7584b54ba3314d0ea73b01f1ec3
alghi-98 <alghi.putraaaa02@gmail.com>
2021-04-27 22:25:11 +0700

    first commit

---
+++ divisi.js
@@ -0,0 +1,9 @@
+ function changeGambarML(){
+ 	document.getElementById("gambardivisi").src="assets/image/ml.jpg";
+ 	document.getElementById("division_title").innerHTML = "Mobile Legend";
+ }
+
+ function changeGambarPUBG(){
+ 	document.getElementById("gambardivisi").src="assets/image/pubg.jpg";
+ 	document.getElementById("division_title").innerHTML = "PUBG ";
+ }
